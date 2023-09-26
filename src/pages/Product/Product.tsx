c5b22123f821f827f068ed69e05ddcbbc4bf7267
Ihor <34889161+Ihor-Kh@users.noreply.github.com>
2023-09-26 11:23:31 +0300

    product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,4 +1,4 @@
-// import styles from './Product.module.css';
+import styles from './Product.module.css';
 
 import { Await, useLoaderData, useParams } from "react-router-dom";
 import { Product } from "../../interfaces/product.interface.ts";
@@ -6,26 +6,43 @@ import { Suspense } from "react";
 
 function Product() {
 	const { id } = useParams()
-	const data = useLoaderData() as { data: Product }
-	// console.log(data)
+	const { data } = useLoaderData() as { data: Product }
+	console.log(id)
 	return (
-		<>
-			<div>
-				id: { id }
-				<br/>
-				<br/>
-				{/*{ data.name }*/ }
 
+		<Suspense fallback={ <>Loading...</> }>
+			<Await resolve={ data }>
+				{ ({ data }: { data: Product }) => (
+					<div>
+						<div className={ styles.img_item } style={ { backgroundImage: 'url("/img/bg_card.png")' } }/>
+						<div>
+							<div>
+								<div>Цена</div>
+								<div>{ data.price } ₴</div>
+							</div>
+							<div>
+								<div>Рейтинг</div>
+								<div className={ styles.rating }>
+									{ data.rating }
+									<img src="/img/star.svg" alt="Рейтинг"/>
+								</div>
+							</div>
+							<div>
+								<div>Состав</div>
+								<ul>
+								{
+									data.ingredients.map((ingredient, index) => (
+										<li key={ index }>{ ingredient }</li>
+									))
+								}
+								</ul>
+							</div>
+						</div>
+					</div>
+				) }
+			</Await>
+		</Suspense>
 
-			</div>
-			<Suspense fallback={ <>Loading...</> }>
-				<Await resolve={ data.data }>
-					{ ({ data }: { data: Product }) => (
-						<>Product - { data.name }</>
-					) }
-				</Await>
-			</Suspense>
-		</>
 	);
 }
 
